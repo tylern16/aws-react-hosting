66ac315a2d5da1e104520186f63cb64d770b26a7
Tyler Nenninger <tylernenninger@Annas-Air.lan>
2023-01-06 11:54:25 -0600

    api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+const url = 'https://api.api-ninjas.com/v1/exercises?muscle=biceps'
+
+const config = {
+  headers:{
+    'X-Api-Key': '7gnw8gOd8L/b9qSrLCRZ8A==EsqlCY0XRIff0uYl'
+  }
+}
 
 function App() {
+  const [exercises, setExercises] = useState([])
+
+  const getExercises = () => {
+    axios.get(url, config)
+    .then(res => setExercises(res.data))
+    .catch(err => console.log(err))
+  }
+
+  getExercises()
+
+  
+
   return (
     <div className="App">
-      Hello World
+      <h1>Get Swole</h1>
+      {exercises.map((exercise) => {
+        return (
+          <div>{exercise.name}</div>
+        )
+      })}
     </div>
   )
 }
 
 export default App;
+
+// 7gnw8gOd8L/b9qSrLCRZ8A==EsqlCY0XRIff0uYl
